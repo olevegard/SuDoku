442a48130c42a1dfc61ef53c6fa5a2b0d268f09e
morslilleole <oleveg@gmail.com>
2013-01-20 18:31:35 +0100

    Added untracked files

---
+++ Digit.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include "Vector2d.h"
+
+class Digit
+{
+	public:
+		Digit()
+			:	m_oPosition( -1, -1 )
+			,	m_iDigit(-1)
+			,	m_bSolved( false )
+		{
+		}
+		Digit( vector2d pos )
+			:	m_oPosition( -1, -1 )
+			,	m_iDigit(-1)
+			,	m_bSolved( false )
+		{
+			m_oPosition = pos;
+		}
+		Digit( vector2d pos, short iDigit, bool bSolved)
+			:	m_oPosition( -1, -1 )
+			,	m_iDigit(-1)
+			,	m_bSolved( false )
+		{
+			m_oPosition = pos;
+			m_iDigit = iDigit;
+			m_bSolved = bSolved;
+		}
+
+		vector2d getPosition() const
+		{
+			return m_oPosition;
+		}
+
+		short getDigit() const
+		{
+			return m_iDigit;
+		}
+
+		bool isSolved() const
+		{
+			return m_bSolved;
+		}
+		
+		bool operator==( const Digit &digit ) const
+		{
+			return digit.getPosition() == m_oPosition;
+		}
+
+		void setAsSolved( short iDigit )
+		{
+			m_iDigit = iDigit;
+			m_bSolved = true;
+		}
+	private:
+		vector2d m_oPosition;
+		short m_iDigit;
+		bool m_bSolved;
+};
+
+
+inline std::ostream& operator<<(std::ostream& stream, const Digit& digit )
+{
+	stream << "Digit ( " << digit.getPosition() << " ) | Solved : ";
+
+	if ( digit.isSolved() )
+		stream << "Yes | Value : " << digit.getDigit() + 1 ;
+	else 
+		stream << "No";
+
+	return stream;
+}
+
---
+++ LogTool.h
@@ -0,0 +1,17 @@
+class CLogTool
+{
+
+	public:
+		// Logs the differece between two poits in time.
+		static void LogTime( const timespec &startTime, const timespec &stopTime )
+		{
+			unsigned long long iTimeStart = startTime.tv_sec * 1000000000 + startTime.tv_nsec;
+			unsigned long long iTimeStop = stopTime.tv_sec * 1000000000 + stopTime.tv_nsec;
+			unsigned long long iTimeDelta = iTimeStop - iTimeStart;
+
+			std::cout << "====================Time used : ====================\n"
+				<< "||\tMillisecods  : " << static_cast< float > ( iTimeDelta / 1000000.0f ) << " ms\t||\n"
+				<< "||\tMicroseconds : " << static_cast< float > ( iTimeDelta / 1000.0f ) << " µs\t||\n"
+				<< "||\tNanoseconds  : " << static_cast< float > ( iTimeDelta ) << " ns\t||\n";
+		}
+};
